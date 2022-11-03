efbe4862717330aca7ac148db2820db527ab6619
NIU1600760 <1600760@uab.cat>
2022-11-03 12:56:50 +0100

    Començament funcio Cronometro

--- src/Cronometro.java
+++ src/Cronometro.java
@@ -12,16 +12,34 @@ class Cronometro extends Observable {
     }
 
 
-    public Cronometro(boolean Inici){
+    public Cronometro(){
         int hora = 0;
         int minutos = 0;
         int segundos = 0;
-        while
+        string min="", seg="", hora="";
+        try{
+            while(Pausar){
+                //mientras CronActivo sea verdadero seguira aumentando el tiempo
+                while(CronActivo){
+                    thread.sleep(1000) //se incrementa 1 segundo
+                    segundos += 1;
+                    if(segundos == 60){
+                        segundos = 0;
+                        minutos++;
+                    }
+                    if (minutos == 60){
+                        minutos == 0;
+                        hora++;
+                    }
+                }
+            }
+        }
 
     }
 
     boolean Inici;
-    boolean Parar;
+    boolean CronActivo
+    boolean Pausar;
     boolean Finalizar;
     int sec;
     int min;
